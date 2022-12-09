3692e3b9e43dfcae55b13f9d7f4c93ef8fa7d73c
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-12-09 07:51:34 -0600

    test passes in the terminal

--- tests/test_download.py
+++ tests/test_download.py
@@ -4,8 +4,8 @@ from selenium import webdriver
 import tests.constants as self
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_download_files():
     options = ChromeOptions()
@@ -20,9 +20,9 @@ def test_download_files():
     lt_options["w3c"] = True;
     lt_options["plugin"] = "python-python";
     options.set_capability('LT:Options', lt_options);
-    
     # Specify the location I want the PDF file to be downloaded to.
-    options.add_experimental_option("prefs", {"download.default_directory": "C:/SeleniumDownloadedFiles"})
+    prefs = {"download.default_directory": "C:\SeleniumDownloadedFiles"};
+    options.add_experimental_option("prefs", prefs);
     
     # LambdaTest Profile username
     user_name = os.environ.get('LT_USERNAME')
@@ -33,9 +33,11 @@ def test_download_files():
     
     # Download the PDF file.
     driver.get("https://www.lambdatest.com/selenium-playground/table-data-download-demo")
-    pdf_button = driver.find_element(By.XPATH, '//*[@id="example_wrapper"]/div[1]/a[4]')
+    # pdf_button = driver.find_element(By.XPATH, '//*[@id="example_wrapper"]/div[1]/a[4]')
+    pdf_button = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="example_wrapper"]/div[1]/a[4]')))
     pdf_button.click()
-    print("successfully downloaded the pdf file to DownloadedFiles folder")
+    # print("successfully downloaded the pdf file to DownloadedFiles folder")
+    print("Succussfully downloaded the pdf file")
     
     
     driver.close()
