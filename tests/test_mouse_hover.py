3648ce2ec53cf6abed7a7bbf55ced87ce46ece86
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-12-09 07:54:08 -0600

    test passes in the terminal

--- tests/test_mouse_hover.py
+++ tests/test_mouse_hover.py
@@ -1,10 +1,7 @@
 import os
 import pytest
 from selenium import webdriver
-import tests.constants as self
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver import ActionChains
 
