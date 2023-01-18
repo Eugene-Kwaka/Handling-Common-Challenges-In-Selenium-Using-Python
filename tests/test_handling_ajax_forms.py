b91457088fc4dd5ce905c72b66db145d8ea574c9
Eugene-Kwaka <kwakaeugene@gmail.com>
2023-01-18 16:45:32 -0600

    Test passes

--- tests/test_handling_ajax_forms.py
+++ tests/test_handling_ajax_forms.py
@@ -1,11 +1,12 @@
 import os
 import pytest
 from selenium import webdriver
-import tests.constants as self
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.select import Select
+
 
 def test_ajax_forms():
     options = ChromeOptions()
@@ -28,4 +29,30 @@ def test_ajax_forms():
     remote_url =  "https://" + user_name + ":" + accesskey + "@hub.lambdatest.com/wd/hub"
     driver = webdriver.Remote(remote_url, options=options)
     
-    driver.get("")
+    driver.get("https://codepen.io/getform/pen/jRoexL")
+    
+    # Get into the iFrame 
+    driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
+    
+    # Locate the Form
+    form = driver.find_element(By.XPATH, '//*[@id="ajaxForm"]')
+    # Identify the forms inputs and enter data through send_keys  
+    email_element = form.find_element(By.XPATH, '//*[@id="exampleInputEmail1"]')
+    email_element.send_keys('myemail@gmail.com')
+    name_element = form.find_element(By.XPATH, '//*[@id="exampleInputName"]')
+    name_element.send_keys('Eugene')
+    # Locate the dropdown 
+    platform_element = Select(form.find_element(By.XPATH, '//*[@id="exampleFormControlSelect1"]'))
+    platform_element.select_by_visible_text('Github')
+    submit = form.find_element(By.XPATH, '//*[@id="ajaxForm"]/button')
+    submit.click()
+    
+    # Wait for the ajax request to complete
+    try:
+        response = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ajaxForm"]/div[4]')))
+        print(response.text)
+    
+    finally:
+        driver.quit()
+        
+    
